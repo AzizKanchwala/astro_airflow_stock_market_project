27966ec055c05becddd279544e7050f68ece67e7
AzizKanchwala <azizkanch435@gmail.com>
2025-05-18 11:40:31 +0530

    Add is_api_available task

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,5 +1,7 @@
-from airflow.decorators import dag
+from airflow.decorators import dag, task # type: ignore
 from datetime import datetime
+from airflow.sensors.base import PokeReturnValue #type: ignore
+from airflow.hooks.base import BaseHook #type: ignore
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -7,6 +9,20 @@ from datetime import datetime
     catchup=False,
     tags=['stock_market'])
 def stock_market():
-    pass
+    
+    @task.sensor(poke_interval=30, timeout=300, mode='poke')
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        
+        # if condition is None, the api is available.
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done=condition, xcom_value=url)
+    
+    is_api_available()
 
 stock_market()
